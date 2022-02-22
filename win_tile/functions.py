88ed0d9c864d171f3f6ea148b10cc49ac2744f41
LinJuz <simeallie@protonmail.com>
2022-02-21 21:15:46 -0500

    fixed monitor_list_resolution function

--- win_tile/functions.py
+++ win_tile/functions.py
@@ -2,22 +2,25 @@ import pygetwindow as pw
 import screeninfo as si
 
 
-"""open_windows function returns a list of all open windows"""
 def open_windows():
-    exclusions = ["Program Manager", "Microsoft Text Input Application"]
-    windows = pw.getAllTitles()
+    """open_windows function returns a list of all open windows"""
 
+    exclusions = ["Program Manager", "Microsoft Text Input Application"]
     win_list = []
+
+    windows = pw.getAllTitles()
     for i, x in enumerate(windows):
         if len(x) > 1 and x not in exclusions:
             win_list.append(x)
+
     return win_list
 
 
-"""window_resize resizes a window given the name of the window to any size"""
 def window_resize():
-    WIDTH = 1920
-    HEIGHT = 1080
+    """window_resize resizes a window given the name of the window to any size"""
+
+    WIDTH = 1920  # These are hard-coded for now; change this later
+    HEIGHT = 1080  # These are hard-coded for now; change this later
 
     win = pw.getWindowsWithTitle("Notepad")[0]
 
@@ -25,17 +28,22 @@ def window_resize():
     win.moveTo(-7, 0)  # -7 we found to be optimal padding
 
 
-"""which_monitor_window returns the (x,y) coordinates of where a window is.
-This will be used when determing on which monitor the window exists on"""
 def which_monitor_window():
+    """which_monitor_window returns the (x,y) coordinates of where a window is.
+    This will be used when determing on which monitor the window exists on"""
+
     win = pw.getWindowsWithTitle("Notepad")[0]
+
     return win.topleft[0]
 
 
 def monitor_list_resolution():
+    """monitor_list_resolution returns all monitors widths and heights as lists with matching indexes"""
     displays = si.get_monitors()
-    for display in range(len(displays)):
-        yield displays[display]
+    display_heights = [display.height for display in displays]
+    display_widths = [display.width for display in displays]
+
+    return display_widths, display_heights 
 
 
 if __name__ == "__main__":
